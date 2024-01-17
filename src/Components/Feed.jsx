7a1efd709c93869ad693dfb0554376e7cf56bbb0
SawantAchal <achals1718@gmail.com>
2024-01-18 00:42:02 +0530

    ADDED A SIDE BAR COMPOMEMY

---
+++ src/Components/Feed.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SideBar from './SideBar'
+
+const Feed = () => {
+  return (
+    <div className='flex flex-row h-[calc(100%-56px)]'>
+      <SideBar />
+    </div>
+  )
+}
+
+export default Feed
