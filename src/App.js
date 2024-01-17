1f6718fe5a039de8af223e55bd1feed85b20df4f
SawantAchal <achals1718@gmail.com>
2024-01-17 21:09:33 +0530

    add all routes to the file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Navbar from './Components/Navbar';
 import { AppComponent } from './Context/contextApi';
+import Feed from './Components/Feed'
+import SearchResult from './Components/SearchResult'
+import VideoDetails from './Components/VideoDetails'
+import Header from './Components/Header'
 
 function App() {
   return (
     <>
       <AppComponent>
-
+        <BrowserRouter>
+          <div>
+            <Header/>
+            <Routes>
+              <Route path='/' exact element={<Feed />}/>
+              <Route path='/searchResult/:searchQuery' element={< SearchResult/>}/>
+              <Route path='/video/:id' element={<VideoDetails />}/>
+            </Routes>
+          </div>
+        </BrowserRouter>
       </AppComponent>
     </>
   );
