03d28acaf436c7ae12f8e0c7db89cb6dc9dbd5d4
SawantAchal <achals1718@gmail.com>
2024-01-18 13:23:16 +0530

    complete with feed file

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,62 +1,33 @@
-// import React, { useContext,  useEffect } from 'react'
-// import SideBar from './SideBar'
-// import { Context } from '../Context/contextApi'
-// import VideoCard from './VideoCard'
-
-// const Feed = () => {
-//   const {loading ,searchResults} = useContext(Context);
-
-//   useEffect(() => {
-//     document.getElementById('root').classList.remove('custom-h')
-//   },[])
-
-//   return (
-//     <div className='flex flex-row h-[calc(100%-56px)]'>
-//       <SideBar />
-//       <div className='grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black'>
-//         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 p-5'>
-//           {!loading && searchResults.map((item )=> {
-//             if(item.type !== 'video') return false;
-//             return (
-//               <VideoCard key={item?.video?.videoId} video={item?.video} />
-//             )
-//           })}
-//         </div>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default Feed
-
-
-
 import React, { useContext, useEffect } from 'react';
 import SideBar from './SideBar';
 import { Context } from '../Context/contextApi';
 import VideoCard from './VideoCard';
 
-const Feed = ({video}) => {
+const Feed = () => {
   const { loading, searchResults } = useContext(Context);
 
   useEffect(() => {
     document.getElementById('root').classList.remove('custom-h');
-  }, []); // Empty dependency array ensures the effect runs only once on mount
+  }, []); 
 
   return (
-    <div className='flex flex-row h-[calc(100%-56px)]'>
-      <SideBar className='hidden'/>
-      <div className='grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black'>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 p-5'>
-          {!loading && searchResults && searchResults?.map((item) => {
-            if (item?.type !== 'video') return false;
-            return (
-              <VideoCard key={item?.video?.videoId} video={item?.video} />
-            );
-          })}
+    <div className="flex flex-row h-[calc(100%-56px)]">
+    <SideBar />
+    <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5">
+            {!loading && searchResults &&
+                searchResults.map((item) => {
+                    if (item.type !== "video") return false;
+                    return (
+                        <VideoCard
+                            key={item?.video?.videoId}
+                            video={item?.video}
+                        />
+                    );
+                })}
         </div>
-      </div>
     </div>
+</div>
   );
 };
 
