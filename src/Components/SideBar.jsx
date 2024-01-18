fbf5ec1caf87710c13abb9126027d9a365f9c923
SawantAchal <achals1718@gmail.com>
2024-01-18 14:01:00 +0530

    complete with  sidebar   page

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import {settingSideBar, sideBar} from '../data/Sidebar'
+import { sideBar} from '../data/Sidebar'
 import { Context } from '../Context/contextApi'
 import SiderBarMenuItem from './SiderBarMenuItem'
 import { useNavigate } from 'react-router-dom'
@@ -22,28 +22,33 @@ const SideBar = () => {
     }
   }
   return (
-    <div className='md:block w-[240px] overflow-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[240px] md:translate-x-0 transition-all text-white'>
+    <div  className={`md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240px] md:translate-x-0 transition-all ${
+      mobileMenu ? "translate-x-0" : ""}`}>
       <div className='flex px-5 flex-col'>
         {
           sideBar.map((item) => {
             // className='flex gap-2 items-center mb-5  hover:bg-gray-400 hover:p-3 hover:rounded-lg'
             return(
-              <div>
+              <div key={item.name}>
                 <SiderBarMenuItem text={item.type === 'home' ? 'Home' : item.name}
                 icon={item.icon}
                 action={() =>{clickHandler(item.name , item.type); naviagte('/')}}
-                className={`${selecteCategories === item.name ? "bg-white/[0.15]" :""}`}/>
-                {item.divider && (
-                  <hr className='my-5 border-white'/>
-                )}
+                className={`${
+                  selecteCategories === item.name
+                      ? "bg-white/[0.15]"
+                      : ""
+              }`}/>
+                            {item.divider && (
+                                <hr className="my-5 border-white/[0.2]" />
+                            )}
               </div>
             )
           })
         }
-        <hr className='my-5 border-white'/>
-        <div>
-          <p className='mt-4'>Clone By : Achal Sawant</p>
-        </div>
+                <hr className="my-5 border-white/[0.2]" />
+                <div className="text-white/[0.5] text-[12px]">
+                    Clone by: ACHAL SAWANt
+                </div>
       </div>
     </div>
   )
